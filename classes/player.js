fc935e4ab9d9cdc4bc13d4a9f96c741e2d155daf
Zequi Aguirre <zequi4real@gmail.com>
2022-08-27 21:54:21 -0400

    no sound and water around

--- classes/player.js
+++ classes/player.js
@@ -12,7 +12,7 @@ class Player {
     let hitShip = this.ships.filter((ship) =>
       ship.location.includes(coordinates)
     );
-    
+
     if (coordinates.classList.contains("ship")) {
       this.shipsToHit--;
       coordinates.classList.add("hit");
@@ -20,51 +20,47 @@ class Player {
       let theMsg = document.querySelector("#messageArea");
       theMsg.innerText = `It's a hit!`;
       hitShip[0].receiveDamage();
-      this.playSound('hit');
+      // this.playSound("hit");
       if (this.shipsToHit === 0) {
         let theMsg = document.querySelector("#messageArea");
         theMsg.innerText = `You sank all my battleships in ${this.attempts} attempts.`;
 
         this.gameBoard.players.forEach((player) => {
-          
           let currentPlayerIndex;
           for (let i = 0; i < this.gameBoard.players.length; i++) {
             if (this.gameBoard.players[i].name === this.name) {
               currentPlayerIndex = i;
             }
           }
-          
+
           let lastPlayerIndex = this.gameBoard.players.length - 1;
           if (this.gameBoard.players[lastPlayerIndex].name !== this.name) {
-            this.gameBoard.players[currentPlayerIndex + 1].displayBoard();
-            document.getElementById("currentPlayer").innerText =
-              this.gameBoard.players[1].name;
+            $("#endPlayer1Modal").modal("show");
+
             document.getElementById("player1Attempts").innerText =
               this.attempts;
-            
-              setTimeout(function () {
-            document.getElementById("messageArea").innerText = '';
-          }, 2000);
 
-             
-              
-            $("#endPlayer1Modal").modal("show");
+            setTimeout(function () {
+              this.gameBoard.players[currentPlayerIndex + 1].displayBoard();
+              document.getElementById("currentPlayer").innerText =
+                this.gameBoard.players[1].name;
+              document.getElementById("messageArea").innerText = "";
+            }, 2000);
           } else if (
             this.gameBoard.players[lastPlayerIndex].name === this.name
           ) {
-
-            
             let player1 = this.gameBoard.players[0];
             let player2 = this.gameBoard.players[1];
 
-            if(player1.attempts === player2.attempts) {
-
-            let gameTied = document.getElementById('endOfGameTie');
-            gameTied.innerText = `This game ends in a tie. Play again?`;
-            $("#tiedGameModal").modal("show");
-
+            if (player1.attempts === player2.attempts) {
+              let gameTied = document.getElementById("endOfGameTie");
+              gameTied.innerText = `This game ends in a tie. Play again?`;
+              $("#tiedGameModal").modal("show");
             } else {
-              let winner = player1.attempts < player2.attempts ? player1.name : player2.name;
+              let winner =
+                player1.attempts < player2.attempts
+                  ? player1.name
+                  : player2.name;
 
               let GameOverTitle = document.getElementById("endOfGame");
               GameOverTitle.innerText = `${winner} is the winner!`;
@@ -75,10 +71,8 @@ class Player {
               let p2Summary = document.getElementById("player2Summary");
               p2Summary.innerText = `${player2.name} sank all battleships in ${player2.attempts} attempts.`;
 
-            $("#endPlayer2Modal").modal("show");
+              $("#endPlayer2Modal").modal("show");
             }
-            
-            
           }
         });
       }
@@ -86,8 +80,8 @@ class Player {
       coordinates.classList.add("miss");
       let theMsg = document.querySelector("#messageArea");
       theMsg.innerText = `It's a miss.`;
-      this.playSound('miss');
-    } 
+      // this.playSound("miss");
+    }
   }
 
   displayBoard() {
@@ -100,16 +94,16 @@ class Player {
 
   playSound(event) {
     let audio;
-    let miss = new Audio('./assets/mixkit-sea-mine-deep-explosion-1186.wav');
-    let hit = new Audio('./assets/mixkit-explosive-impact-from-afar-2758.wav');
+    let miss = new Audio("./assets/mixkit-sea-mine-deep-explosion-1186.wav");
+    let hit = new Audio("./assets/mixkit-explosive-impact-from-afar-2758.wav");
     switch (event) {
-      case 'miss':
+      case "miss":
         audio = miss;
         break;
-      case 'hit':
+      case "hit":
         audio = hit;
         break;
     }
-   audio.play();
+    audio.play();
   }
 }
